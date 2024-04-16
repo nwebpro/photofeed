cd9207661dd05677c26f54a0f3da9eb6733839d0
nwebpro <abnaeem.bsc@gmail.com>
2024-04-16 16:54:16 +0600

    photo details added

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,12 +1,12 @@
-import { getDictionary } from "./disctionaries";
+import PhotoList from '@/components/PhotoList';
+import { getDictionary } from './disctionaries';
 
-const Home = async ({params: {lang}}) => {
-	const dictionary = await getDictionary(lang)
-	return (
-		<div>
-			{dictionary.followers}
-		</div>
-	);
+const Home = async ({ params: { lang } }) => {
+	const dictionary = await getDictionary(lang);
+	const photosData = await fetch(`${process.env.BASE_API_URL}/photos`);
+	const photos = await photosData.json();
+
+	return <PhotoList photos={photos} />;
 };
 
 export default Home;
